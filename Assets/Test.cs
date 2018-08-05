544740592260113610da65be5f68bf6c7443b014
RyokoAraki <a-ry.27@poem.ocn.ne.jp>
2018-08-05 23:24:47 +0900

    ScriptTestプロジェクト　再提出
    mpが足りているか判定するタイミングを変更。11回目にmpが足りないメッセージが表示されるよう修正。

--- Assets/Test.cs
+++ Assets/Test.cs
@@ -28,14 +28,17 @@ public class Test : MonoBehaviour {
         // mpを消費して魔法攻撃をするMagic関数
         public void Magic()
         {
-            mp -= 5;
             Debug.Log("魔法攻撃をした。残りMPは" + mp + "。");
 
-            if (mp < 5) {
+            if (mp < 5)
+            {
 
                 Debug.Log("MPが足りないため魔法が使えない。");
 
             }
+
+            mp -= 5;
+           
         }
 
     }
@@ -69,7 +72,7 @@ public class Test : MonoBehaviour {
 
         Boss lastboss = new Boss();
 
-        for (int i = 0; i < 10; i++)
+        for (int i = 0; i < 11; i++)
         {
             lastboss.Magic();
         }
